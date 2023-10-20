8d28224c42ff448dbc49d031ea8c4aaf8dbe2bb6
Miguel Maldonado <miguel.maldonado5802@gmail.com>
2023-10-20 11:20:46 -0400

    adding in the base files. Working on db. Put in tables in the schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db
+
+USE employee_db
+
+-- table for department
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+)
+
+-- table for employee role
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id)
+    ON DELETE SET NULL
+)
+
+-- table for employee. Trying to see how to have it reference it self for manager assignment
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    FOREIGN KEY (role_id),
+    REFERENCES role(id),
+    manager_id INT
+    FOREIGN key (manager_id),
+    REFERENCES employee(id),
+    ON DELETE SET NULL
+)
